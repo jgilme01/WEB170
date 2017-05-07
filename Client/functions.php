0a9a5b7ee35eb67496acd0f97203f4091b524062
jgilme01 <jgilme01@seattlecentral.edu>
2017-05-07 11:40:00 -0700

    basic wordpress integration into theme

---
+++ Client/functions.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Theme Name: JGilme WordPress Demo
+Author: JGilme
+Author URI: http://www.dohmang.com/
+Description: This is my demo theme for the WEB170 WordPress class.
+Version: 1.0
+
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+function style_enqueue_style() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() ); 
+}
+add_action( 'wp_enqueue_scripts', 'style_enqueue_style' );
+
+?>
---
+++ Client/index.php
@@ -0,0 +1,228 @@
+<?php
+
+?>
+
+<HTML>
+<HEAD>
+<TITLE><?php bloginfo('description');?> : <?php bloginfo('name');?>Rolfing Seattle</TITLE>
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>"><link rel="stylesheet" media="all" href="scripts/icwebsiteelement.css">
+<meta name="robots" content="index, follow"><meta name="description" content="Rolfing Seattle, Stephen Evanko, PhD, LMP is a Certified Rolfer in North Seattle."><meta name="keywords" content="Rolfing,structural integration,repetitive stress injury,Seattle,Northwest,lower back pain,posture,chronic pain,chronic fatigue,syndrome,self improvement,pain relief,rheumatoid arthritis,chronic fatigue syndrome,injury,low back pain,alignment,back injury,carpal tunnel syndrome,ergonomics,body work,chronic back pain,sports injury,back pain,joint pain,massage therapy,knee injury,holistic health,shoulder injury,fascia,tmj,arthritis,carpal tunnel,scoliosis"><META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE"><META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+<script language="JavaScript" src="<?php bloginfo('template_directory');?>../scripts/ic_globals_published.js"></script><script language="JavaScript" src="<?php bloginfo('template_directory');?>scripts/user.js"></script><script language="JavaScript" src="<?php bloginfo('template_directory');?>scripts/photoalbum.js"></script>
+<SCRIPT language="JavaScript">
+<!--
+
+function newImage(arg) {
+	if (document.images) {
+		rslt = new Image();
+		rslt.src = arg;
+		return rslt;
+	}
+}
+
+function changeImages() {
+	if (document.images && (preloadFlag == true)) {
+		for (var i=0; i<changeImages.arguments.length; i+=2) {
+			document[changeImages.arguments[i]].src = changeImages.arguments[i+1];
+		}
+	}
+}
+
+var preloadFlag = false;
+function preloadImages() {
+	if (document.images) {
+		fre_06_over = newImage("images/fre_06-over.jpg");
+		fre_08_over = newImage("images/fre_08-over.jpg");
+		fre_09_over = newImage("images/fre_09-over.jpg");
+		fre_10_over = newImage("images/fre_10-over.jpg");
+		fre_11_over = newImage("images/fre_11-over.jpg");
+		fre_12_over = newImage("images/fre_12-over.jpg");
+		preloadFlag = true;
+	}
+}
+
+// -->
+</SCRIPT>
+    
+<?php wp_head();?>
+    
+</HEAD>
+<BODY <?php body_class();?> bgcolor=#FFFFFF topmargin=0 leftmargin=0 marginwidth=0 marginheight=0 background="images/expandbg.gif"><script language="JavaScript">preloadImages();</script>
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0" background="images/expandtop.gif">
+  <tr>
+    <td>
+      <table border=0 cellpadding=0 cellspacing=0>
+        <tr>
+          <td> <img src="<?php bloginfo('template_directory');?>/images/spacer.gif" width=151 height=1></td>
+          <td> <img src="<?php bloginfo('template_directory');?>/images/spacer.gif" width=18 height=1></td>
+          <td> <img src="<?php bloginfo('template_directory');?>/images/spacer.gif" width=47 height=1></td>
+          <td> <img src="<?php bloginfo('template_directory');?>/images/spacer.gif" width=394 height=1></td>
+        </tr>
+        <tr>
+          <td colspan=2 rowspan=3> <img src="<?php bloginfo('template_directory');?>/images/fre_01.jpg" width=169 height=100></td>
+          <td colspan=2> <img src="<?php bloginfo('template_directory');?>/images/company.jpg" width=441 height=37></td>
+        </tr>
+        <tr>
+          <td colspan=2> <img src="<?php bloginfo('template_directory');?>/images/slogan.jpg" width=441 height=25></td>
+        </tr>
+        <tr>
+          <td> <img src="<?php bloginfo('template_directory');?>/images/fre_04.gif" width=47 height=38></td>
+          <td> <img src="<?php bloginfo('template_directory');?>/images/fre_05.gif" width=394 height=38></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td valign="top" width="10%">
+      <table width="151" border=0 cellpadding=0 cellspacing=0 background="images/new_back.jpg">
+        <tr>
+          <td>
+            <table width="151" border=0 cellpadding=0 cellspacing=0 background="images/spacer.gif">
+              <!-- Start buttons section --><tr><td> <a href="/home.html"			   onMouseOver="changeImages("/home.html", "images/01b.jpg"); return true;"
+			   onMouseOut="changeImages("/home.html", "images/01a.jpg"); return true;" target="_self"> 
+                  <img name="/home.html" src="<?php bloginfo('template_directory');?>/images/01a.jpg" width=151 height=40 border=0></a></td></tr><tr><td> <a href="/benefitsofrolfing.html"			   onMouseOver="changeImages("/benefitsofrolfing.html", "images/02b.jpg"); return true;"
+			   onMouseOut="changeImages("/benefitsofrolfing.html", "images/02a.jpg"); return true;" target="_self"> 
+                  <img name="/benefitsofrolfing.html" src="<?php bloginfo('template_directory');?>/images/02a.jpg" width=151 height=40 border=0></a></td></tr><tr><td> <a href="/tenseriesoverview.html"			   onMouseOver="changeImages("/tenseriesoverview.html", "images/03b.jpg"); return true;"
+			   onMouseOut="changeImages("/tenseriesoverview.html", "images/03a.jpg"); return true;" target="_self"> 
+                  <img name="/tenseriesoverview.html" src="<?php bloginfo('template_directory');?>/images/03a.jpg" width=151 height=40 border=0></a></td></tr><tr><td> <a href="/faq.html"			   onMouseOver="changeImages("/faq.html", "images/04b.jpg"); return true;"
+			   onMouseOut="changeImages("/faq.html", "images/04a.jpg"); return true;" target="_self"> 
+                  <img name="/faq.html" src="<?php bloginfo('template_directory');?>/images/04a.jpg" width=151 height=40 border=0></a></td></tr><tr><td> <a href="/aboutfascia.html"			   onMouseOver="changeImages("/aboutfascia.html", "images/05b.jpg"); return true;"
+			   onMouseOut="changeImages("/aboutfascia.html", "images/05a.jpg"); return true;" target="_self"> 
+                  <img name="/aboutfascia.html" src="<?php bloginfo('template_directory');?>/images/05a.jpg" width=151 height=40 border=0></a></td></tr><tr><td><img name="spacer" src="<?php bloginfo('template_directory');?>/images/spacer.gif" width=151 height=40 border=0></td></tr><!-- End buttons section -->
+              <tr>
+                <td> <img src="<?php bloginfo('template_directory');?>/images/fre_13.jpg" width=151 height=40></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+    <td width="0%" valign="top"><img src="<?php bloginfo('template_directory');?>/images/clear.gif" width="18" height="1">
+    </td>
+    <td width="100%" valign="top">
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr valign="top">
+          <td><!-- Start console section --><!-- End console section -->
+              
+              
+              <!-- Start content section --><P>
+              
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
+<h2><a href="<?php the_permalink(); // link to the page or posting ?>"><?php the_title(); // get the page or posting title ?></a></h2>
+<?php the_content(''); // get page or posting written content ?>
+<?php endwhile; endif; // end the loop ?>
+              
+              
+<table border="0" width="100%" cellpadding="0" cellspacing="0">
+<tr>
+<td>
+<table border="0" name="" width="100%" cellpadding="5" cellspacing="0">
+<tr align="" valign="">
+<td colspan="1" rowspan="1" width="100%" align="" valign="top">              <div><strong><span size="6" editor_id="mce_editor_0" style="font-size: xx-large;">Rolfing&reg; Structural Integration &amp; Neurofascial&nbsp;Integration</span></strong>&nbsp;</div><br>           
+</td>
+</tr>
+</table>
+<table border="0" name="" width="100%" cellpadding="5" cellspacing="0">
+<tr align="" valign="">
+<td colspan="1" rowspan="1" width="33%" align="left" valign="top">              <table summary="image table" cellpadding="0" cellspacing="0" border="0" width="162" style=""><tr>
+<td align="left" valign="top">
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+<tr>
+<td align="center"><img width="137" src="<?php bloginfo('template_directory');?>/images/137_steve-203x173.jpg" alt="" style="" border="0" height="116"></td>
+</tr>
+<tr>
+<td align="center"> </td>
+</tr>
+</table>
+</td>
+</tr></table>           </td><td colspan="1" rowspan="1" width="67%" align="" valign="top">              Stephen Evanko, PhD, Certified Advanced Rolfer ™, LMP (WA # MA 00012182) 
+<P></P>
+<P>&nbsp;Welcome! I hope this website will answer most of your questions about Rolfing® Structural Integration. <BR>
+Please call (206) 368-4352 or email by copying and pasting: steveandlin@comcast.net for more information or to set up an appointment. <BR><BR>My office is conveniently located in the Northgate area of Seattle at 11524 15th Ave NE, Suite B.<BR>Daytime, evening and Saturday appointment times are available.<BR><BR>The word "Rolfing®" is a service mark of the Rolf Institute, Boulder, CO.</P><br>                        
+</td>
+</tr>
+</table>
+<table border="0" name="" width="100%" cellpadding="5" cellspacing="0">
+<tr align="" valign="">
+<td colspan="1" rowspan="1" width="50%" align="" valign="top">              <STRONG><FONT size=4>What Is Rolfing?</FONT></STRONG><BR>Rolfing® Structural Integration is a process of physical manipulation and movement education developed over 50 years of study by Dr. Ida Rolf. Dr. Rolf"s genius was her recognition that the body can be changed by manipulating connective tissue, and that it is the entire body"s relationship to gravity that is most important. Rolfing is a sophisticated, scientifically validated system designed to reorganize the body and enhance its patterns of shape and movement into a more graceful and efficient relationship with gravity. Rolfing balances the entire body and increases freedom of movement, thus resolving chronic problems and improving function, by releasing and realigning the body’s major segments (legs, torso, shoulders, arms, head) which have succumbed to lifelong patterns of tension and holding.<BR><BR>Over a series of sessions, the Rolfer and client work together to establish changes in the form, balance, and function of the body. In conjunction with movement by the client, the Rolfer slowly lengthens and repositions the body’s fascia. This frees up trapped and aggravated nerves, restores normal length, elasticity and fluidity to the connective tissues and muscle, and provides more freedom in the joints. In my practice, freeing tethered and irritated nerves is always on the forefront throughout the process. Because nerves rule the situation by controlling myofascial tension, range of motion, transmitting pain signals and driving inflammation, neural mobilization effects greater structural change and pain relief than regular myofascial work, and also aids in resetting sympathetic/parasympathetic balance.<BR><BR><A HREF="tenseriesoverview.html" TARGET="_self" >more on the "ten series"</A><BR><BR><br>                        
+</td><td colspan="1" rowspan="1" width="50%" align="left" valign="top">              <table summary="image table" cellpadding="0" cellspacing="0" border="0" width="253" style=""><tr>
+<td align="left" valign="top">
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+<tr>
+<td align="center"><img width="228" src="<?php bloginfo('template_directory');?>/images/228_Ida_Rolfing.jpg" alt="" style="" border="0" height="355"></td>
+</tr>
+<tr>
+<td align="center">Dr. Ida P. Rolf</td>
+</tr>
+</table>
+</td>
+</tr></table>           </td>
+</tr>
+</table>
+<table border="0" name="" width="100%" cellpadding="5" cellspacing="0">
+<tr align="" valign="">
+<td colspan="1" rowspan="1" width="25%" align="left" valign="top">              <table summary="image table" cellpadding="0" cellspacing="0" border="0" width="119" style=""><tr>
+<td align="left" valign="top">
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+<tr>
+<td align="center"><img width="94" src="<?php bloginfo('template_directory');?>/images/94_logo-small-106x176.jpg" alt="" style="" border="0" height="156"></td>
+</tr>
+<tr>
+<td align="center"></td>
+</tr>
+</table>
+</td>
+</tr></table>           </td><td colspan="1" rowspan="1" width="75%" align="" valign="top">              <div>Structural Integration&nbsp;addresses the whole human being. A human being is more than a collection of parts. One basic factor of well-being is how the parts fit and work together as a whole, and how that body relates to the gravity field of the earth. Although you may feel pain or tension only in particular areas of your body, there are significant compensations in the fascia throughout your structure. The WHOLE body must be brought back into balance to effect long lasting change and release. <br /><br />Our bodies act as storage systems for the accumulated physical and emotional events of our lives. Injury, illness or emotional traumas leave imprints on our structure, which are seen as strain patterns in the myofascial connective tissue network. The connective tissue becomes tight, bunched up and fibrous to compensate for the stress that it feels due to chronic muscle tensions. When the tissue becomes sticky and stiff, mobility&nbsp;and alignment are&nbsp;decreased, nerves become trapped,&nbsp;and pain ensues, forcing us to function with more limited options in movement. This wastes energy. This, in turn, affects the way we feel and the way we present ourselves in the world. <br /><br />Rolfers are trained in body reading and can see the strain patterns&nbsp;and relationships that affect&nbsp;the whole body. For example,&nbsp;strain and counter-rotations in your feet and legs (i.e. in your base of support - such as more pronation of one foot) can influence the tilt of your pelvis&nbsp;and thus the low back. Twisting of the shoulder girdle and strain in the arms can affect the neck and thorax.&nbsp;<span style="font-size: 10pt;">Rolfing and neurofascial integration restore mobility and fluidity to the connective tissues and nerves, and promote well-being by enhancing the body&rsquo;s self-organizing capabilities. Once better alignment and tensional balance is achieved, gravity helps to give us a lift instead of pulling us more out of balance.&nbsp;</span></div><br>                        
+</td>
+</tr>
+</table>
+<table border="0" name="" width="100%" cellpadding="5" cellspacing="0">
+<tr align="" valign="">
+<td colspan="1" rowspan="1" width="67%" align="" valign="top">              &gt;Steve\"s Bio</FONT></STRONG><BR>I received my PhD in 1993 from the University of New Mexico where I studied the biological properties and biochemistry of connective tissues, such as fascia, tendons, ligaments and cartilage. My main focus was how the tissues remodeled in response to different mechanical stresses. When I first experienced Rolfing SI in 1995, I was so impressed with the profound results that I decided to further explore this powerful process and make Rolfing my new career. This was real application of what I was studying in the laboratory! I was certified as a Rolfer in 1998 by the Rolf Institute in Boulder, CO. I received my Advanced Certification in 2007. 
+<P></P>
+<P>I am excited about the addition of nerve manipulation to my practice. This is designed to free up tethered nerves and diminish neurogenic inflammation, and adds an exciting new dimension of possibility for pain relief, structural changes and improved function. </P>
+<P>I continue to do research on connective tissue on a part-time basis. My recent studies include experiments on the effects of manipulation at the cellular level, and factors that drive fibrosis and inflammation.<A HREF="aboutfascia.html" TARGET="_self" >What is Fascia?</A></P><br>                        
+</td><td colspan="1" rowspan="1" width="33%" align="left" valign="top">
+	  &nbsp;
+        </td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</P><table border="0" width="100%" cellpadding="0" cellspacing="0" id="other_footer">
+<tr>
+<td align="left" valign="middle"></td>
+</tr>
+<tr>
+
+</tr>
+</table>
+<!-- End content section --></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td valign="top" width="10%">&nbsp;</td>
+    <td width="0%" valign="top">&nbsp;</td>
+    <td width="100%" valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td valign="top" width="10%">&nbsp;</td>
+    <td width="0%" valign="top">&nbsp;</td>
+    <td width="100%" align="center">
+      <div align="center"></div>
+    </td>
+  </tr>
+</table>
+    
+<footer>
+    </footer>
+
+    <?php wp_footer();?>
+
+</body>
+</html>
